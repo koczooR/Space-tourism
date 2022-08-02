e101cc54e20613399f7f0c6b6a55e05db4865baa
koczooR <koczorowski94@gmail.com>
2022-08-02 23:14:48 +0200

    home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,17 @@
 export const Home = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <section className="home">
+      <div className="home_text_box">
+        <p className="home_text_box-text1">SO, YOU WANT TO TRAVEL TO</p>
+        <h1 className="home_text_box-title">SPACE</h1>
+        <p className="home_text_box-text2">
+          Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and
+          relax because we’ll give you a truly out of this world experience!
+        </p>
+      </div>
+      <div className="home_btn">
+        <a href="/">EXPLORE</a>
+      </div>
+    </section>
   );
 };
