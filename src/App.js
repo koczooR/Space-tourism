d3feb7477bf1c1362c2fced2862d05911919a39a
koczooR <koczorowski94@gmail.com>
2022-08-03 00:56:31 +0200

    nav

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Crew } from "./components/Crew/Crew";
 import { Destination } from "./components/Destination/Destination";
 import { Home } from "./components/Home/Home";
+import { Nav } from "./components/Home/Nav";
 import { Technology } from "./components/Technology/Technology";
 
 export function App() {
   return (
     <Router>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/destination" element={<Destination />} />
---
+++ src/components/Home/Nav.js
@@ -0,0 +1,33 @@
+import logo from "../../assets/shared/logo.svg";
+
+export const Nav = () => {
+  return (
+    <header className="header">
+      <img src={logo} alt="logo" />
+      <nav>
+        <ul>
+          <li>
+            <a href="/">
+              <span>00</span> HOME
+            </a>
+          </li>
+          <li>
+            <a href="/destination">
+              <span>01</span> DESTINATION
+            </a>
+          </li>
+          <li>
+            <a href="/crew">
+              <span>02</span> CREW
+            </a>
+          </li>
+          <li>
+            <a href="/technology">
+              <span>03</span> TECHNOLOGY
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
